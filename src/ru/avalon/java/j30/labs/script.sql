4f1a74da6e379a68eafa3df36f5d562af81ef68e
VlaTam <tvs1273@gmail.com>
2019-05-07 17:16:44 +0300

    first

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -4,3 +4,88 @@
  * 2. Опишите все необходимые отношения между таблицами.
  * 3. Добавьте в каждую таблицу минимум три записи.
  */
+
+CREATE schema lab1;
+
+create table roles (
+      id integer,
+      name varchar(255),
+                   constraint pk_role_name primary key (name),
+                   constraint uq_role_id unique (id),
+                   constraint ck_role_id check ( id < 10000000000 and id > 0)
+);
+
+create table userinfo (
+                        id integer,
+                        name varchar(255) not null,
+                        surname varchar(255) not null,
+                        constraint pk_userinfo primary key (id)
+);
+
+create table user (
+      id integer,
+      email varchar(255),
+      password varchar(255),
+      info integer,
+      role integer,
+                    constraint pk_email primary key (email),
+                    constraint uq_user_id unique (id),
+                    constraint ck_user_id check ( id < 10000000000 and id > 0),
+                    constraint uq_user_info unique (id),
+                    constraint ck_user_info check ( info < 10000000000 and info > 0),
+                    constraint fk_user_userinfo_id  foreign key (info)
+                                                    references userinfo(id),
+                    constraint ck_user_role check ( role < 10000000000 and role > 0),
+                    constraint fk_user_role_id  foreign key (role)
+                                                references roles(id)
+);
+
+create table order
+(
+      id integer,
+      user integer,
+      created timestamp,
+                    constraint pk_name primary key (id),
+                    constraint ck_order_id check ( id < 10000000000 and id > 0),
+                    constraint ck_order_user check ( order.user < 10000000000 and order.user > 0),
+                    constraint fk_order_user_id foreign key (user)
+                                                references user(id)
+);
+
+create table order2product (
+      order integer,
+      product integer,
+                    constraint pk_order2product primary key (order, product),
+                    constraint ck_order2product_order check ( order2product.order < 10000000000 and order2product.order > 0),
+                    constraint ck_oorder2product_product check ( product < 10000000000 and product > 0),
+                    constraint fk_order2product_order_id  foreign key (order)
+                                                          references order(id)
+/*!!!!*/
+);
+
+
+create table product (
+    id integer,
+    code varchar(255),
+    title varchar(255),
+    supplier integer,
+    initial_price double,
+    retail_value double,
+    constraint pk_product primary key (code),
+    constraint uq_product_id unique (id),
+)
+
+
+
+
+create table supplier (
+      id integer,
+      name varchar(255),
+      address varchar(255) not null,
+      phone varchar(255),
+      representative varchar(255) not null,
+                    constraint pk_name primary key (name),
+                    constraint uq_id unique (id),
+                    constraint ck_supplier_id check ( id < 10000000000 and id > 0)
+);
+
