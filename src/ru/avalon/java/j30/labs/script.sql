a933b9014b9276e4e4d22ee21c36482ef1dacd27
VlaTam <tvs1273@gmail.com>
2019-05-10 15:38:22 +0300

    final

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -7,27 +7,27 @@
 
 CREATE schema lab1;
 
-create table roles (
-      id integer,
-      name varchar(255),
-                   constraint pk_role_name primary key (name),
-                   constraint uq_role_id unique (id),
-                   constraint ck_role_id check ( id < 10000000000 and id > 0)
+CREATE TABLE roles (
+     id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
+     name varchar(255),
+               constraint pk_role_name primary key (name),
+               constraint uq_role_id unique (id),
+               constraint ck_role_id check ( id < 10000000000 and id > 0)
 );
 
-create table userinfo (
-                        id integer,
-                        name varchar(255) not null,
-                        surname varchar(255) not null,
-                        constraint pk_userinfo primary key (id)
+CREATE TABLE userinfo (
+    id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
+    name varchar(255), /*not null,*/
+    surname varchar(255), /*not null,*/
+              constraint pk_userinfo primary key (id)
 );
 
-create table users (
-      id integer,
+CREATE TABLE users (   /*user - зарезервированное слово */
+      id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
       email varchar(255),
-      password varchar(255),
-      info integer,
-      role integer,
+      password varchar(255) NOT NULL,
+      info integer NOT NULL,
+      role integer NOT NULL,
                     constraint pk_email primary key (email),
                     constraint uq_user_id unique (id),
                     constraint ck_user_id check ( id < 10000000000 and id > 0),
@@ -40,11 +40,11 @@ create table users (
                                                 references roles(id)
 );
 
-create table orders
+CREATE TABLE orders   /*order - зарезервированное слово */
 (
-      id integer,
-      customer integer,
-      created timestamp,
+      id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
+      customer integer,   /*user - зарезервированное слово */
+      created timestamp CONSTRAINT 'DD-MON-YYYY HH24:MI:SS' NOT NULL,
                     constraint pk_name primary key (id),
                     constraint ck_order_id check ( id < 10000000000 and id > 0),
                     constraint ck_order_user check ( customer < 10000000000 and customer > 0),
@@ -52,27 +52,27 @@ create table orders
                                                 references users(id)
 );
 
-create table supplier (
-      id integer,
+CREATE TABLE supplier (
+      id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
       name varchar(255),
-      address varchar(255) not null,
-      phone varchar(255),
-      representative varchar(255) not null,
+      address varchar(255), /*not null,*/
+      phone varchar(255) NOT NULL,
+      representative varchar(255), /*not null,*/
                     constraint pk_supplier_name primary key (name),
                     constraint uq_supplier_id unique (id),
                     constraint ck_supplier_id check ( id < 10000000000 and id > 0)
 );
 
-create table product (
-      id integer,
+CREATE TABLE product (
+      id integer GENERATED ALWAYS AS IDENTITY ( START WITH 1, INCREMENT BY 1),
       code varchar(255),
-      title varchar(255),
+      title varchar(255) NOT NULL,
       supplier integer,
-      initial_price double,
-      retail_value double,
+      initial_price double NOT NULL,
+      retail_value double NOT NULL,
                    constraint pk_product primary key (code),
                    constraint uq_product_id unique (id),
-                   constraint ck_product_id check ( product < 10000000000 and product > 0),
+                   constraint ck_product_id check ( id < 10000000000 and id > 0),
                    constraint ck_product_supplier check ( supplier < 10000000000 and supplier > 0),
                    constraint fk_product_supplier foreign key (supplier)
                                                   references supplier(id),
@@ -80,18 +80,105 @@ create table product (
                    constraint ck_product_retail_value check ( retail_value < 10000000000 and retail_value > 0)
 );
 
-create table order2product (
-      order integer,
+CREATE TABLE order2product (
+      order_id integer,   /*order - зарезервированное слово */
       product integer,
-                    constraint pk_order2product primary key (order, product),
-                    constraint ck_order2product_order check ( order2product.order < 10000000000 and order2product.order > 0),
+                    constraint pk_order2product primary key (order_id, product),
+                    constraint ck_order2product_order_id check ( order_id < 10000000000 and order_id > 0),
                     constraint ck_oorder2product_product check ( product < 10000000000 and product > 0),
-                    constraint fk_order2product_order_id  foreign key (order)
+                    constraint fk_order2product_order_id  foreign key (order_id)
                                                           references orders (id),
-                    constraint fk_order2product_product_id  foreign key (order)
+                    constraint fk_order2product_product  foreign key (product)
                                                             references product(id)
 );
 
 
+INSERT INTO roles(name)
+VALUES ('admin');
 
+INSERT INTO roles(name)
+VALUES ('manager');
+
+INSERT INTO roles(name)
+VALUES ('customer');
+
+
+    INSERT INTO userinfo(name, surname)
+    VALUES ('Ivan', 'Petrov');
+
+    INSERT INTO userinfo(name, surname)
+    VALUES ('Irina', 'Ivanova');
+
+    INSERT INTO userinfo(name, surname)
+    VALUES ('Vladimir', 'Sidorov');
+
+
+        INSERT INTO users(email, password, info, role)
+        VALUES ('first@gmail.com', 'first5555', 1, 1);
+
+        INSERT INTO users(email, password, info, role)
+        VALUES ('second@gmail.com', 'second1276', 2, 2);
+
+        INSERT INTO users(email, password, info, role)
+        VALUES ('third@gmail.com', 'third9453', 3, 3);
+
+
+            INSERT INTO orders(customer, created)
+            VALUES (1, CURRENT TIMESTAMP);
+
+            INSERT INTO orders(customer, created)
+            VALUES (1, CURRENT TIMESTAMP);
+
+            INSERT INTO orders(customer, created)
+            VALUES (2, CURRENT TIMESTAMP);
+
+
+                INSERT INTO supplier(name, address, phone, representative)
+                VALUES ('Vzlet', 'SPB, Komsomola 4', '5555512', 'Nosov');
+
+                INSERT INTO supplier(name, address, phone, representative)
+                VALUES ('Omega', 'SPB, Nevskyi', '1234567', 'Nekrasov');
+
+                INSERT INTO supplier(name, address, phone, representative)
+                VALUES ('Smart devices', 'SPB, Leninskyi 42', '1238712', 'Sergeev');
+
+
+                    INSERT INTO product(code, title, supplier, initial_price, retail_value)
+                    VALUES ('ff12', 'iphone 8', 3, 35000, 45000);
+
+                    INSERT INTO product(code, title, supplier, initial_price, retail_value)
+                    VALUES ('ff14', 'sony vaio', 1, 50000, 71000);
+
+                    INSERT INTO product(code, title, supplier, initial_price, retail_value)
+                    VALUES ('gs21', 'mouse smartbuy', 4, 500, 1000);
+
+                        INSERT INTO order2product(order_id, product)
+                        VALUES (2, 1);
+
+                        INSERT INTO order2product(order_id, product)
+                        VALUES (3, 4);
+
+                        INSERT INTO order2product(order_id, product)
+                        VALUES (4, 6);
+
+SELECT *
+FROM roles;
+
+SELECT *
+FROM userinfo;
+
+SELECT *
+FROM users;
+
+SELECT *
+FROM orders;
+
+SELECT *
+FROM supplier;
+
+SELECT *
+FROM product;
+
+SELECT *
+FROM order2product;
 
